107d6a23a4ff16eb113c76181d22e14cc9f8203a
reboucasdi <diegoreboucas.santana3@gmail.com>
2024-10-08 01:48:13 -0300

    Conclusão testes e validações com frontend

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -8,13 +8,15 @@ namespace GerenciadorTarefas.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O título da tarefa é obrigatório.")]
-        public string Title { get; set; } = string.Empty; // Inicializando como string vazia para evitar null
+        public string Titulo { get; set; } = string.Empty; // Inicializando como string vazia para evitar null
 
         [Required(ErrorMessage = "A descrição da tarefa é obrigatória.")]
-        public string Description { get; set; } = string.Empty; // Inicializando como string vazia para evitar null
+        public string Descricao { get; set; } = string.Empty; // Inicializando como string vazia para evitar null
 
-        public bool Completed { get; set; } = false; // Por padrão, uma tarefa é não concluída
+        public bool Status { get; set; } = false; // Por padrão, uma tarefa é não concluída
 
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Data de criação da tarefa
+        public DateTime DataCriacao { get; set; } = DateTime.UtcNow; // Data de criação da tarefa
+
+        public DateTime DataEdicao { get; set; } // Adicionar campo DataEdicao (nulo se nunca editado)
     }
 }
